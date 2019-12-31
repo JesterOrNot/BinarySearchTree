736c3327630953adce256586d59d1dcf975a67dd
Sean Hellum <seanhellum45@gmail.com>
2019-12-31 04:17:08 +0000

    fix printers

--- BST.rs
+++ BST.rs
@@ -26,28 +26,28 @@ impl<T: Display> BinaryTree<T> {
     pub fn print_pre_order(&self, root: &TreeNode<T>) {
         print!("{}, ", root.value);
         if let Some(left) = &root.left {
-            self.print_in_order(left);
+            self.print_pre_order(left);
         }
         if let Some(right) = &root.right {
-            self.print_in_order(right);
+            self.print_pre_order(right);
         }
     }
     pub fn print_post_order(&self, root: &TreeNode<T>) {
         if let Some(left) = &root.left {
-            self.print_in_order(left);
+            self.print_post_order(left);
         }
         if let Some(right) = &root.right {
-            self.print_in_order(right);
+            self.print_post_order(right);
         }
         print!("{}, ", root.value);
     }
     pub fn print_out_order(&self, root: &TreeNode<T>) {
         if let Some(right) = &root.right {
-            self.print_in_order(right);
+            self.print_out_order(right);
         }
         print!("{}, ", root.value);
         if let Some(left) = &root.left {
-            self.print_in_order(left);
+            self.print_out_order(left);
         }
     }
 }
