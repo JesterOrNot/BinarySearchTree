151b4b0e9d8327dbec674f3ccb6069af8a31c288
Sean Hellum <seanhellum45@gmail.com>
2019-12-31 04:09:19 +0000

    add print (out,pre,post) order functions

--- BST.rs
+++ BST.rs
@@ -23,6 +23,33 @@ impl<T: Display> BinaryTree<T> {
             self.print_in_order(right);
         }
     }
+    pub fn print_pre_order(&self, root: &TreeNode<T>) {
+        print!("{}, ", root.value);
+        if let Some(left) = &root.left {
+            self.print_in_order(left);
+        }
+        if let Some(right) = &root.right {
+            self.print_in_order(right);
+        }
+    }
+    pub fn print_post_order(&self, root: &TreeNode<T>) {
+        if let Some(left) = &root.left {
+            self.print_in_order(left);
+        }
+        if let Some(right) = &root.right {
+            self.print_in_order(right);
+        }
+        print!("{}, ", root.value);
+    }
+    pub fn print_out_order(&self, root: &TreeNode<T>) {
+        if let Some(right) = &root.right {
+            self.print_in_order(right);
+        }
+        print!("{}, ", root.value);
+        if let Some(left) = &root.left {
+            self.print_in_order(left);
+        }
+    }
 }
 impl<T> TreeNode<T> {
     fn new(val: T) -> Self {
